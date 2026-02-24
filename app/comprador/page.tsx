2720e7a55360e1eae17685310c0e32edf4799d6e
Lucas David <lucasdavidoliveira80@gmail.com>
2026-02-24 16:20:55 -0300

    feat(comprador): adiciona página de dashboard para comprador

---
+++ app/comprador/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/src/components/Header";
+
+
+export default function CompradorDashboard() {
+    return (
+        <main>
+            <Header variant="buyer" />
+            {/* Conteúdo do dashboard... */}
+        </main>
+    )
+}
