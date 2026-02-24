22e3305416233bb2eaf3282aa16118dd87e16117
Lucas David <lucasdavidoliveira80@gmail.com>
2026-02-24 16:21:39 -0300

    feat(fornecedor): adiciona página de dashboard para fornecedor

---
+++ app/fornecedor/page.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/src/components/Header";
+
+
+export default function FornecedorDashboard() {
+    return (
+        <main>
+            <Header variant="supplier" />
+            {/* Conteúdo do dashboard... */}
+        </main>
+    )
+}
