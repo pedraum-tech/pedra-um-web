a4ab71050dffc6e27af2ffb4de7f33194848bfed
Lucas David <lucasdavidoliveira80@gmail.com>
2026-03-02 10:57:53 -0300

    feat: configuracao inicial do firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Puxando as chaves do seu arquivo .env.local
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// O Next.js roda no servidor e no cliente. 
+// Isso garante que o Firebase não tente inicializar duas vezes e cause erros.
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+// Exportando os serviços que vamos usar
+export const auth = getAuth(app);
+export const db = getFirestore(app);
