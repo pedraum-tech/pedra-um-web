c85a7d00b921ae65e4cdfe0d6c7090b9e8b28bfe
Lucas David <lucasdavidoliveira80@gmail.com>
2026-03-05 11:42:11 -0300

    feat(usuario): adiciona interface UsuarioModel para definir o formato da demanda no banco de dados

---
+++ src/types/usuario.ts
@@ -0,0 +1,14 @@
+export type UserRole = "comprador" | "fornecedor" | "admin";
+
+export interface UsuarioModel {
+    id: string; // O UID do Firebase Auth
+    nome?: string;
+    razaoSocial?: string;
+    email: string;
+    telefone?: string;
+    role: UserRole;
+    cnpj?: string;
+    cpf?: string;
+    saldoLeads?: number;
+    dataCadastro: string;
+}
